97f060c0861a509ab3f55e4a817dc212d41a59bd
kyeongho <dagh1218@naver.com>
2023-12-14 00:01:47 +0900

    #3 gradle 기반 spring boot 프로젝트 생성 및 기본 설정 추가
    - java 17, spring-boot:3.1.6 기반 구성
        - DB 구성 및 ORM을 위한 postgres(v16), spring-data-jpa 의존성 추가
        - DB 및 실행 환경 구성용 컨테이너 관리를 위한 docker-compose 의존성 추가
        - 테스트 환경 구성용 컨테이너 관리를 위한 testcontainers 의존성 추가
        - 코드 생산성 향상을 위한 lombok, spring-boot-configuration-processor, devtool 추가
        - web 환경 구성 및 인증/인가를 위한 spring-web, spring-security 의존성 추가
    - 기본 실행 환경 구성
        - docker-compose.yml (postgres:16 컨테이너) 구성
        - db 관련 접근 설정 추가

---
+++ feedbook/src/main/java/com/guardjo/feedbook/FeedbookApplication.java
@@ -0,0 +1,13 @@
+package com.guardjo.feedbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedbookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeedbookApplication.class, args);
+    }
+
+}
