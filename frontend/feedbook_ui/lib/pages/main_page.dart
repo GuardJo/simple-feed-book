c8775ba6f27fb009e8fa9bcc3da08ce296e7405f
kyeongho <dagh1218@naver.com>
2024-02-11 16:37:44 +0900

    #5 사이드 바 메뉴 화면 구현
    - AllFeed, MyFeed, WriteFeed 메뉴 탭 구현
        - Scaffold widget 내 하위 속성 중 drawer 속성을 기반으로 구현
    - 메인 페이지 별도 객체로 분리

---
+++ frontend/feedbook_ui/lib/pages/main_page.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+
+class AllFeedPage extends StatefulWidget {
+  const AllFeedPage({super.key});
+
+  @override
+  State<AllFeedPage> createState() => _AllFeedPageState();
+}
+
+class _AllFeedPageState extends State<AllFeedPage> {
+  final String _appName = "Simple Feed Book";
+
+  PageType pageType = PageType.allFeed;
+
+  void _onTapUpdate(PageType pageType) {
+    setState(() {
+      pageType = pageType;
+    });
+  }
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: _appName,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
+        useMaterial3: true,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(_appName),
+          backgroundColor: Colors.white,
+          shadowColor: const Color(0xFF000000),
+          elevation: 2,
+        ),
+        drawer: Drawer(
+          child: ListView(
+            children: [
+              const DrawerHeader(
+                child: Text("Menu"),
+              ),
+              ListTile(
+                title: const Text(
+                  "All Feeds",
+                ),
+                onTap: () {
+                  _onTapUpdate(PageType.allFeed);
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                title: const Text(
+                  "My Feeds",
+                ),
+                onTap: () {
+                  _onTapUpdate(PageType.allFeed);
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                title: const Text(
+                  "Write Feed",
+                ),
+                onTap: () {
+                  _onTapUpdate(PageType.allFeed);
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+enum PageType {
+  allFeed,
+  myFeed,
+  writeFeed,
+}
