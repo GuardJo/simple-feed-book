ee93939b97c9e6cf2d7e53f4b00dbc6f3fd35e59
kyeongho <dagh1218@naver.com>
2024-02-12 15:34:50 +0900

    #5 로그인 화면 구현
    - 좌측 사이드 메뉴에 로그인 화면 이동 버튼 추가
        - Navigator.push를 통해 LoginPage로 이동하게끔 구현
    - 로그인 화면 내 form 및 로그인 요청 데이터 확인

--- frontend/feedbook_ui/lib/main.dart
+++ frontend/feedbook_ui/lib/main.dart
@@ -1,6 +1,23 @@
-import 'package:feedbook_ui/pages/main_page.dart';
+import 'package:feedbook_ui/pages/main_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const AllFeedPage());
+  runApp(const RootPage());
+}
+
+class RootPage extends StatelessWidget {
+  final String _appName = "Simple Feed Book";
+  const RootPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: _appName,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
+        useMaterial3: true,
+      ),
+      home: const AllFeedPage(),
+    );
+  }
 }
