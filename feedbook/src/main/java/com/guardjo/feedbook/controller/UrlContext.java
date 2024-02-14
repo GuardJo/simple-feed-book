735f455de00596b4d6531a2fecc94bbe09c00211
kyeongho <dagh1218@naver.com>
2024-02-15 01:21:54 +0900

    #6 API 입출력 형식 및 URL 정의
    - 입출력 형식 구현
        - 회원가입 요청 모델인 SignupRequest를 record 객체로 구성
        - 공통 출력 모델인 BaseResponse 제네릭 객체 구성
    - URL 별도 Util 객체로 관리

---
+++ feedbook/src/main/java/com/guardjo/feedbook/controller/UrlContext.java
@@ -0,0 +1,9 @@
+package com.guardjo.feedbook.controller;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class UrlContext {
+	public final static String SIGNUP_URL = "/api/signup";
+}
