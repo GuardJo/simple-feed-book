d2fa17f820a6f504d28e476766b6c4e6ec47e4b6
kyeongho <dagh1218@naver.com>
2024-02-15 01:20:24 +0900

    #6 임시 security 설정 구성
    - 인증/인가 관련 기능 구현 전 임시로 전체 요청에 대해 인증/인가 처리를 하지 않도록 설정
    - 테스트용 security 설정도 추가
    - spring data jpa의 osiv 기본 설정 disable

--- feedbook/src/main/java/com/guardjo/feedbook/config/SecurityConfig.java
+++ feedbook/src/main/java/com/guardjo/feedbook/config/SecurityConfig.java
@@ -2,13 +2,30 @@ package com.guardjo.feedbook.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+		httpSecurity.authorizeHttpRequests(registry -> {
+				registry.anyRequest()
+					.permitAll();
+			})
+			.formLogin(AbstractHttpConfigurer::disable)
+			.csrf(AbstractHttpConfigurer::disable);
+
+		return httpSecurity.build();
 	}
 }
