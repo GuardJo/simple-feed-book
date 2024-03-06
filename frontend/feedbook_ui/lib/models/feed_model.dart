65323210ce3d9c80385508751fb1372f98ab01bd
kyeongho <dagh1218@naver.com>
2024-03-07 00:15:17 +0900

    #14 피드 목록 화면 이동 기능 구현
    - AllFeeds, My Feeds 탭 클릭 시 메인화면을 FeedListWidget으로 변경하도록 구현
    - 피드 데이터 객체 구성

---
+++ frontend/feedbook_ui/lib/models/feed_model.dart
@@ -0,0 +1,12 @@
+class Feed {
+  final String title;
+  final String content;
+  final String author;
+  final bool isOwner;
+
+  Feed(
+      {required this.title,
+      required this.content,
+      required this.author,
+      required this.isOwner});
+}
