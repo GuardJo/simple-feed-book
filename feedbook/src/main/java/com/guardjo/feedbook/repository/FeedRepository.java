9a4256015277f27bba271ba86328b7609019492f
Kyeongho <kyeongho_jo@lotte.net>
2024-03-21 09:20:57 +0900

    #32 사용자별 전체 Feed Entity 목록 조회 쿼리 구현
    - JPA를 통해 Account 별 Feed Entity 목록 조회 쿼리 메소드 구현
    - 관련 테스트 추가

--- feedbook/src/main/java/com/guardjo/feedbook/repository/FeedRepository.java
+++ feedbook/src/main/java/com/guardjo/feedbook/repository/FeedRepository.java
@@ -1,8 +1,11 @@
 package com.guardjo.feedbook.repository;
 
+import com.guardjo.feedbook.model.domain.Account;
+import com.guardjo.feedbook.model.domain.Feed;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.guardjo.feedbook.model.domain.Feed;
-
 public interface FeedRepository extends JpaRepository<Feed, Long> {
+    Page<Feed> findAllByAccount(Pageable pageable, Account account);
 }
