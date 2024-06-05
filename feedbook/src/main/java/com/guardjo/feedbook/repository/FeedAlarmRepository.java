2ea9f4ae4d355fad75d804beb84ac6a10e6de14b
kyeongho <dagh1218@naver.com>
2024-06-06 02:39:17 +0900

    #47 FeedAlarm 제거 쿼리 메소드 개선
    - spring-data-jpa에서 기본적으로 제공하는 DELETE 쿼리 메소드는 우선 관련 Entity를 조회 후 제거하기에 비효율적임
    - 별도 JPQL 쿼리로 선언하여 호출하도록 개선함
        - 이 때 JPQL 로 실행 시에는 Persistence Context를 거치지 않기에 Context를 초기화 후 처리하도록 함

--- feedbook/src/main/java/com/guardjo/feedbook/repository/FeedAlarmRepository.java
+++ feedbook/src/main/java/com/guardjo/feedbook/repository/FeedAlarmRepository.java
@@ -3,11 +3,18 @@ package com.guardjo.feedbook.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.guardjo.feedbook.model.domain.FeedAlarm;
 
 public interface FeedAlarmRepository extends JpaRepository<FeedAlarm, Long> {
-	void deleteAllByFeed_Account_Id(long accountId);
+	@Query(
+		"delete from FeedAlarm fa where fa.feed.account.id = :accountId"
+	)
+	@Modifying(clearAutomatically = true)
+	void deleteAllByFeed_Account_Id(@Param("accountId") long accountId);
 
 	Page<FeedAlarm> findAllByFeed_Account_Id(long accountId, Pageable pageable);
 }
