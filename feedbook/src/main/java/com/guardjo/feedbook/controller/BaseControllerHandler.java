bbcd041dd940a63e04f73d7828aeba2983c6b53d
kyeongho <kyeongho_jo@lotte.net>
2025-04-07 14:42:26 +0900

    [BE] #75 인증/인가 프로세스 간 예외 처리 로직 개선
    - Jwt 유틸 객체에서 던지던 Expired 관련 예외를 밖에서 처리하도록 명시
    - API 응답 간 예외 발생 시 http 상태 코드를 기반으로 반환하도록 개선

--- feedbook/src/main/java/com/guardjo/feedbook/controller/BaseControllerHandler.java
+++ feedbook/src/main/java/com/guardjo/feedbook/controller/BaseControllerHandler.java
@@ -1,76 +1,60 @@
 package com.guardjo.feedbook.controller;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-
-import com.guardjo.feedbook.controller.response.BaseResponse;
 import com.guardjo.feedbook.exception.DuplicateUsernameException;
 import com.guardjo.feedbook.exception.EntityNotFoundException;
 import com.guardjo.feedbook.exception.InvalidRequestException;
 import com.guardjo.feedbook.exception.WrongPasswordException;
-
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice(basePackageClasses = {
-	AccountController.class,
-	FeedController.class
+        AccountController.class,
+        FeedController.class
 })
 @Slf4j
 public class BaseControllerHandler extends ResponseEntityExceptionHandler {
-	@ExceptionHandler(value = {
-		IllegalArgumentException.class
-	})
-	public BaseResponse<String> handleBadRequest(Exception e) {
-		log.warn("Exception : BadRequest, ", e);
-		return BaseResponse.<String>builder()
-			.status(HttpStatus.BAD_REQUEST.name())
-			.body(e.getMessage())
-			.build();
-	}
+    @ExceptionHandler(value = {
+            IllegalArgumentException.class
+    })
+    public ResponseEntity<String> handleBadRequest(Exception e) {
+        log.warn("Exception : BadRequest, ", e);
 
-	@ExceptionHandler(value = {
-		WrongPasswordException.class
-	})
-	public BaseResponse<String> handleUnauthorize(Exception e) {
-		log.warn("Exception : Unauthorize, ", e);
-		return BaseResponse.<String>builder()
-			.status(HttpStatus.UNAUTHORIZED.name())
-			.body(e.getMessage())
-			.build();
-	}
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
 
-	@ExceptionHandler(value = {
-		InvalidRequestException.class
-	})
-	public BaseResponse<String> handleForbidden(Exception e) {
-		log.warn("Exception : Forbidden, ", e);
-		return BaseResponse.<String>builder()
-			.status(HttpStatus.FORBIDDEN.name())
-			.body(e.getMessage())
-			.build();
-	}
+    @ExceptionHandler(value = {
+            WrongPasswordException.class
+    })
+    public ResponseEntity<String> handleUnauthorized(Exception e) {
+        log.warn("Exception : Unauthorize, ", e);
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
+    }
 
-	@ExceptionHandler(value = {
-		EntityNotFoundException.class
-	})
-	public BaseResponse<String> handleNotFound(Exception e) {
-		log.warn("Exception : NotFound, ", e);
-		return BaseResponse.<String>builder()
-			.status(HttpStatus.NOT_FOUND.name())
-			.body(e.getMessage())
-			.build();
-	}
+    @ExceptionHandler(value = {
+            InvalidRequestException.class
+    })
+    public ResponseEntity<String> handleForbidden(Exception e) {
+        log.warn("Exception : Forbidden, ", e);
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(e.getMessage());
+    }
 
-	@ExceptionHandler(value = {
-		DuplicateUsernameException.class
-	})
-	public BaseResponse<String> handleConflict(Exception e) {
-		log.warn("Exception : Conflict, ", e);
-		return BaseResponse.<String>builder()
-			.status(HttpStatus.CONFLICT.name())
-			.body(e.getMessage())
-			.build();
-	}
+    @ExceptionHandler(value = {
+            EntityNotFoundException.class
+    })
+    public ResponseEntity<String> handleNotFound(Exception e) {
+        log.warn("Exception : NotFound, ", e);
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
+
+    @ExceptionHandler(value = {
+            DuplicateUsernameException.class
+    })
+    public ResponseEntity<String> handleConflict(Exception e) {
+        log.warn("Exception : Conflict, ", e);
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
+    }
 }
--- feedbook/src/main/java/com/guardjo/feedbook/util/JwtProvider.java
+++ feedbook/src/main/java/com/guardjo/feedbook/util/JwtProvider.java
@@ -1,85 +1,87 @@
 package com.guardjo.feedbook.util;
 
+import com.guardjo.feedbook.config.JwtProperties;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
 import java.nio.charset.StandardCharsets;
 import java.security.Key;
 import java.util.Date;
 
-import org.springframework.stereotype.Component;
-
-import com.guardjo.feedbook.config.JwtProperties;
-
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.JwtParser;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.security.Keys;
-
 @Component
 public class JwtProvider {
-	private final Key jwtSecret;
-	private final long expiredTime;
-	private final String claimKeyName;
-	private final String headerPrefix;
+    private final Key jwtSecret;
+    private final long expiredTime;
+    private final String claimKeyName;
+    private final String headerPrefix;
 
-	public JwtProvider(JwtProperties jwtProperties) {
-		this.jwtSecret = encodeKey(jwtProperties.secret());
-		this.expiredTime = jwtProperties.expiredTime();
-		this.claimKeyName = jwtProperties.claimKeyName();
-		this.headerPrefix = jwtProperties.headerPrefix();
-	}
+    public JwtProvider(JwtProperties jwtProperties) {
+        this.jwtSecret = encodeKey(jwtProperties.secret());
+        this.expiredTime = jwtProperties.expiredTime();
+        this.claimKeyName = jwtProperties.claimKeyName();
+        this.headerPrefix = jwtProperties.headerPrefix();
+    }
 
-	/**
-	 * 사용자 식별키를 기준으로 JWT 토큰을 발급한다.
-	 * @param username 사용자 식별아이디
-	 * @return JWT 토큰
-	 */
-	public String createToken(String username) {
-		Claims claims = Jwts.claims();
-		claims.put(claimKeyName, username);
-		Date now = new Date();
+    /**
+     * 사용자 식별키를 기준으로 JWT 토큰을 발급한다.
+     *
+     * @param username 사용자 식별아이디
+     * @return JWT 토큰
+     */
+    public String createToken(String username) {
+        Claims claims = Jwts.claims();
+        claims.put(claimKeyName, username);
+        Date now = new Date();
 
-		return Jwts.builder()
-			.claim(claimKeyName, username)
-			.setIssuedAt(now)
-			.setExpiration(new Date(now.getTime() + expiredTime))
-			.signWith(jwtSecret, SignatureAlgorithm.HS256)
-			.compact();
-	}
+        return Jwts.builder()
+                .claim(claimKeyName, username)
+                .setIssuedAt(now)
+                .setExpiration(new Date(now.getTime() + expiredTime))
+                .signWith(jwtSecret, SignatureAlgorithm.HS256)
+                .compact();
+    }
 
-	/**
-	 * 토큰 만료 여부 확인
-	 * @param token 만료 여부 확인할 JWT 토큰
-	 * @return 만료 여부
-	 */
-	public boolean isExpired(String token) {
-		Claims claims = getClaims(token);
+    /**
+     * 토큰 만료 여부 확인
+     *
+     * @param token 만료 여부 확인할 JWT 토큰
+     * @return 만료 여부
+     */
+    public boolean isExpired(String token) {
+        try {
+            Claims claims = getClaims(token);
+            return claims.getExpiration().before(new Date());
 
-		return claims.getExpiration().before(new Date());
-	}
+        } catch (ExpiredJwtException e) {
+            return true;
+        }
+    }
 
-	/**
-	 * 토큰 내 username 조회
-	 * @param token JWT 토큰
-	 * @return 토큰 내 사용자 식별값 반환
-	 */
-	public String getUsername(String token) {
-		Claims claims = getClaims(token);
+    /**
+     * 토큰 내 username 조회
+     *
+     * @param token JWT 토큰
+     * @return 토큰 내 사용자 식별값 반환
+     */
+    public String getUsername(String token) {
+        Claims claims = getClaims(token);
 
-		return (String)claims.get(claimKeyName);
-	}
+        return (String) claims.get(claimKeyName);
+    }
 
-	private Claims getClaims(String token) {
-		token = token.replace(headerPrefix, "");
+    private Claims getClaims(String token) {
+        token = token.replace(headerPrefix, "");
 
-		JwtParser jwtParser = Jwts.parserBuilder()
-			.setSigningKey(this.jwtSecret)
-			.build();
+        JwtParser jwtParser = Jwts.parserBuilder()
+                .setSigningKey(this.jwtSecret)
+                .build();
 
-		return jwtParser.parseClaimsJws(token)
-			.getBody();
-	}
+        return jwtParser.parseClaimsJws(token)
+                .getBody();
+    }
 
-	private Key encodeKey(String plainKey) {
-		return Keys.hmacShaKeyFor(plainKey.getBytes(StandardCharsets.UTF_8));
-	}
+    private Key encodeKey(String plainKey) {
+        return Keys.hmacShaKeyFor(plainKey.getBytes(StandardCharsets.UTF_8));
+    }
 }
